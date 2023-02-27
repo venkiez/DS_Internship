9056e17cb9cfbdee7bef77f5a61705afa05ad538
Venkatesh Mungara <119108160+venkiez@users.noreply.github.com>
2023-02-27 20:06:34 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,2 +1,10 @@
 import streamlit as st
-st.header ("Hello World!")
+
+st.title("Hello! This is Venkatesh")
+st.snow()
+
+btn_click = st.button("Click Me!")
+
+if btn_click == True:
+    st.subheader("You clicked me :cry:")
+    st.balloons()
---
+++ pages/4_Dashboard_Car.py
@@ -0,0 +1,41 @@
+import streamlit as st
+from matplotlib import image
+import pandas as pd
+import plotly.express as px
+import os
+
+st.title("Dashboard - Cars Data")
+
+# absolute path to this file
+FILE_DIR = os.path.dirname(os.path.abspath(__file__))
+# absolute path to this file's root directory
+PARENT_DIR = os.path.join(FILE_DIR, os.pardir)
+# absolute path of directory_of_interest
+dir_of_interest = os.path.join(PARENT_DIR, "resources")
+
+IMAGE_PATH = os.path.join(dir_of_interest, "images", "car.jpg")
+DATA_PATH = os.path.join(dir_of_interest, "data", "car_data.csv")
+
+img = image.imread(IMAGE_PATH)
+st.image(img)
+
+df = pd.read_csv(DATA_PATH)
+st.dataframe(df)
+
+fuel_type = st.selectbox("Select the fuel_type:", df['fuel_type'].unique())
+
+col1, col2 = st.columns(2)
+
+fig_1 = px.histogram(df[df['fuel_type'] == fuel_type], x="fuel_type")
+col1.plotly_chart(fig_1, use_container_width=True)
+
+fig_2 = px.box(df[df['fuel_type'] == fuel_type], y="price")
+col2.plotly_chart(fig_2, use_container_width=True)
+
+col3, col4 = st.columns(2)
+
+fig_3 = px.bar(df['fuel_type'])
+col3.plotly_chart(fig_3, use_container_width=True)
+
+fig_4 = px.scatter(df['price'])
+col4.plotly_chart(fig_4, use_container_width=True)
