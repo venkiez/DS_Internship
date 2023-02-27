68959b8198569709fb4aad98c1d32f1fd9706bb6
Venkatesh Mungara <119108160+venkiez@users.noreply.github.com>
2023-02-27 20:18:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,5 +6,5 @@ st.title("Hello! This is Venkatesh")
 btn_click = st.button("Click Me!")
 
 if btn_click == True:
-    st.subheader("You clicked me :happy:")
+    st.subheader("You clicked me :panda_face:")
     st.balloons()
