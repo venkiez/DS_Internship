ed041bd86de5cb9a4ca7ea7f329e5df19bc283ba
Venkatesh Mungara <119108160+venkiez@users.noreply.github.com>
2023-02-27 20:26:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 import streamlit as st
 
-st.title("Hello! This is Venkatesh")
+st.title('Hello! This is Venkatesh"')
+st.title('A title with _italics_ :blue[colors] and emojis :sunglasses:')
 
 
 btn_click = st.button("Click Me!")
 
 if btn_click == True:
-    st.subheader("You clicked me :panda_face:")
+    st.success('Yay!', icon="✅")
     st.balloons()
