a9205d1a495bf2ad448b4260a6093e7e2cbfdbc2
Venkatesh Mungara <119108160+venkiez@users.noreply.github.com>
2023-02-27 20:10:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import streamlit as st
 
 st.title("Hello! This is Venkatesh")
-st.snow()
+
 
 btn_click = st.button("Click Me!")
 
 if btn_click == True:
-    st.subheader("You clicked me :cry:")
+    st.subheader("You clicked me :happy:")
     st.balloons()
