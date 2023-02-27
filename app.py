ac004757bc993ff43ce96c0f1281cefd3aeca526
Venkatesh Mungara <119108160+venkiez@users.noreply.github.com>
2023-02-27 20:28:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title('Hello! This is Venkatesh' :blue[colors] and emojis :sunglasses:')
+st.title('Hello! This is Venkatesh :blue[colors] and emojis :sunglasses:')
 
 
 btn_click = st.button("Click Me!")
